5706760544c1ee055d8ff6fff9c75c86d42b90b1
Sandhya <sandhyasuresh03242@gmail.com>
2025-08-05 12:47:33 +0530

    Updating timestamp

--- main_agent.py
+++ main_agent.py
@@ -83,14 +83,14 @@ async def notify(request: Request):
     title=payload.get("title",'')
     description=payload.get("description","")
     timestamp=payload.get("timestamp",'')
-    try:
-        ist_time = datetime.fromisoformat(timestamp)
-        formatted_time = ist_time.strftime("%Y-%m-%d %H:%M:%S %z")
-    except Exception:
-        formatted_time = timestamp
+    # try:
+    #     ist_time = datetime.fromisoformat(timestamp)
+    #     formatted_time = ist_time.strftime("%Y-%m-%d %H:%M:%S %z")
+    # except Exception:
+    #     formatted_time = timestamp
 
     message = f"🔔 New GitHub event: {event_type} on repository: {repo}"
-    message+=f"\n- Title: {title}\n- Description: {description}\n- Timestamp: {formatted_time}\n- User: {sender}\n"
+    message+=f"\n- Title: {title}\n- Description: {description}\n- Timestamp: {timestamp}\n- User: {sender}\n"
     print(message)
     # state={
     #     "messages":[
